8f866b6f6805ab4c089f7586c7a36cd3d6d1717a
Hendrik Makait <hendrik.makait@gmail.com>
2020-04-02 10:06:23 +0200

    Use message in producer.send()

--- include/rembrandt/producer/producer.h
+++ include/rembrandt/producer/producer.h
@@ -1,10 +1,11 @@
 #ifndef REMBRANDT_SRC_PRODUCER_PRODUCER_H_
 #define REMBRANDT_SRC_PRODUCER_PRODUCER_H_
 
+#include <rembrandt/network/message.h>
 #include "rembrandt/producer/producer_config.h"
 class Producer {
  public:
-  virtual void Send(TopicPartition topic_partition, void *buffer, size_t length) = 0;
+  virtual void Send(TopicPartition topic_partition, std::unique_ptr<Message> message) = 0;
 };
 
 #endif //REMBRANDT_SRC_PRODUCER_PRODUCER_H_
