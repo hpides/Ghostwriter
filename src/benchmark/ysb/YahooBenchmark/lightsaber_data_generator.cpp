e21f878f436bc176e45196afc99091c70c44b18f
Hendrik Makait <hendrik.makait@gmail.com>
2022-06-23 21:20:33 +0200

    Simplify Lightsaber data generator

--- src/benchmark/ysb/YahooBenchmark/lightsaber_data_generator.cpp
+++ src/benchmark/ysb/YahooBenchmark/lightsaber_data_generator.cpp
@@ -27,21 +27,6 @@ struct record {
 };
 
 
-void shuffle(record* array, size_t n)
-{
-  if (n > 1)
-  {
-    size_t i;
-    for (i = 0; i < n - 1; i++)
-    {
-      size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
-      record t = array[j];
-      array[j] = array[i];
-      array[i] = t;
-    }
-  }
-}
-
 void generate(record& data, size_t ad_id, size_t page_id, size_t user_id, size_t index)
 {
   auto ts = std::chrono::system_clock::now().time_since_epoch();
@@ -61,7 +46,6 @@ int main(int argc, char *argv[])
 
   char* filePath = "data.bin";
   size_t tupleCnt = 1000;
-  size_t threadCnt = 1;
 
   if(argc == 3)
   {
@@ -76,7 +60,6 @@ int main(int argc, char *argv[])
 
   std::string path(filePath);
   path = path.substr(0,path.find("."));
-  path.append(to_string(i));
   path.append(".bin");
 
   std::random_device rd;  //Will be used to obtain a seed for the random number engine
@@ -87,44 +70,18 @@ int main(int argc, char *argv[])
   std::uniform_int_distribution<size_t> ads_distribution(0, 1000);
   std::uniform_int_distribution<size_t> distr(0, 1000000);
 
-  record** recs = new record*[threadCnt];
-  for(size_t i = 0; i < threadCnt; i++)
+  record* recs = new record[tupleCnt];
+  for(size_t i = 0; i < tupleCnt; i++)
   {
     ad_ids[i] = ads_distribution(gen);
     page_ids[i] = distr(gen);
     user_ids[i] = distr(gen);
-    recs[i] = new record[tupleCnt];
+    generate(recs[i], ad_ids[i], page_ids[i], user_ids[i], i);
   }
-
-#pragma omp parallel num_threads(threadCnt)
-  {
-#pragma omp for
-    for(size_t th = 0; th < threadCnt; th++)
-    {
-      for(size_t i = 0; i < tupleCnt; i++)
-      {
-        generate(recs[omp_get_thread_num()][i], ad_ids[i], page_ids[i], user_ids[i], i);
-        //cout << "thread=" << omp_get_thread_num() << " put in " << i << " value=" << recs[omp_get_thread_num()][i].event_type << endl;
-      }
-
-//      shuffle(recs[omp_get_thread_num()], tupleCnt);
 
       //write to file
-      cout << "writing file" << fileNames[omp_get_thread_num()] << endl;
-      std::ofstream ofp(fileNames[omp_get_thread_num()], std::ios::out | std::ios::binary);
-      ofp.write(reinterpret_cast<const char*>(recs[omp_get_thread_num()]), tupleCnt * sizeof(record));
-      ofp.close();
-
-      //DBG read first 10
-      //		record* inRecs = new record[tupleCnt];
-      //		std::ifstream ifp(fileNames[omp_get_thread_num()], std::ios::in | std::ios::binary);
-      //		ifp.read(reinterpret_cast<char*>(inRecs), tupleCnt * sizeof(record));
-      //		for(size_t i = 0; i < tupleCnt; i++)
-      //		{
-      //			cout << "eventtype " << i << "=" << inRecs[i].event_type << endl;
-      //		}
-      //		ifp.close();
-    }
-
-  }
+  cout << "writing file" << path << endl;
+  std::ofstream ofp(path, std::ios::out | std::ios::binary);
+  ofp.write(reinterpret_cast<const char*>(recs), tupleCnt * sizeof(record));
+  ofp.close();
 }
