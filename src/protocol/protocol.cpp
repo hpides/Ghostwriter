79d45646d416f956069aa8814130cf5f5c4fa68e
Hendrik Makait <hendrik.makait@gmail.com>
2022-07-14 19:17:43 +0200

    Clean CMake

--- src/protocol/protocol.cpp
+++ src/protocol/protocol.cpp
@@ -4,6 +4,8 @@ size_t Protocol::GetEffectiveBatchSize(size_t batch_size, Partition::Mode mode) 
         switch (mode) {
             case Partition::Mode::EXCLUSIVE: return batch_size;
             case Partition::Mode::CONCURRENT: return Protocol::GetConcurrentBatchSize(batch_size);
+            default: throw std::runtime_error("Partition mode not available!");
+
         }
 }
 
