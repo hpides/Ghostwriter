41947274e640c68bb11cdcb23989ad5e388ff499
Hendrik Makait <hendrik.makait@gmail.com>
2022-09-26 20:28:26 +0200

    Adjust Kafka benchmark producer

--- include/rembrandt/benchmark/throughput/kafka/producer.h
+++ include/rembrandt/benchmark/throughput/kafka/producer.h
@@ -3,11 +3,21 @@
 
 #include <memory>
 #include <unordered_set>
+#include <rdkafkacpp.h>
 #include <tbb/concurrent_hash_map.h>
 #include <tbb/concurrent_queue.h>
-#include <rembrandt/producer/direct_producer.h>
-#include <rembrandt/network/ucx/context.h>
 #include <rembrandt/benchmark/common/parallel_data_generator.h>
+
+struct ProducerConfig {
+  std::string broker_node_ip = "10.10.0.1";
+  uint16_t broker_node_port = 13360;
+  size_t send_buffer_size = 16;
+  size_t max_batch_size;
+  size_t data_size = 1024l * 1024 * 1024 * 80;
+  float warmup_fraction = 0.1;
+  size_t rate_limit = 1000l * 1000 * 1000 * 10;
+  std::string log_directory = "/tmp/log/kafka";
+};
 
 class BenchmarkProducer{
  public:
@@ -16,19 +26,22 @@ class BenchmarkProducer{
 
  private:
   void ParseOptions(int argc, char *const *argv);
+  void ConfigureKafka();
   void Warmup();
   size_t GetBatchCount();
   size_t GetRunBatchCount();
   size_t GetWarmupBatchCount();
-  size_t GetEffectiveBatchSize();
   ProducerConfig config_;
-  std::unique_ptr<UCP::Context> context_p_;
-  std::unique_ptr<Producer> producer_p_;
+  RdKafka::Conf *kconfig_;
+
+  std::atomic<bool> running_;
+  std::unique_ptr<RdKafka::Producer> producer_p_;
   std::unique_ptr<std::unordered_set<std::unique_ptr<char>>> buffers_p_;
   std::unique_ptr<tbb::concurrent_bounded_queue<char *>> free_buffers_p_;
   std::unique_ptr<tbb::concurrent_bounded_queue<char *>> generated_buffers_p_;
   std::unique_ptr<ParallelDataGenerator> warmup_generator_p_;
   std::unique_ptr<ParallelDataGenerator> generator_p_;
+  std::unique_ptr<std::thread> polling_thread_p_;
 };
 
 #endif //REMBRANDT_INCLUDE_REMBRANDT_BENCHMARK_PRODUCER_H_
--- src/benchmark/throughput/kafka/producer.cpp
+++ src/benchmark/throughput/kafka/producer.cpp
@@ -1,23 +1,87 @@
 #include <iostream>
 #include <boost/program_options.hpp>
-#include <rembrandt/benchmark/throughput/ghostwriter/producer.h>
+#include <rdkafkacpp.h>
+#include <rembrandt/benchmark/throughput/kafka/producer.h>
 #include <rembrandt/logging/throughput_logger.h>
-#include <rembrandt/network/attached_message.h>
-#include <rembrandt/protocol/protocol.h>
+#include <rembrandt/logging/latency_logger.h> // needed?
+
+class ThroughputLoggingDeliveryReportCb : public RdKafka::DeliveryReportCb {
+ public:
+  explicit ThroughputLoggingDeliveryReportCb(std::atomic<long> &counter,
+                                          tbb::concurrent_bounded_queue<char *> &free_buffers) :
+      counter_(counter),
+      free_buffers_(free_buffers) {}
+  void dr_cb(RdKafka::Message &message) override {
+    if (message.err()) {
+      exit(1);
+    } else {
+      ++counter_;
+      free_buffers_.push((char *) message.msg_opaque());
+    }
+  }
+ private:
+  std::atomic<long> &counter_;
+  tbb::concurrent_bounded_queue<char *> &free_buffers_;
+};
+
+class LatencyLoggingDeliveryReportCb : public RdKafka::DeliveryReportCb {
+ public:
+  explicit LatencyLoggingDeliveryReportCb(std::atomic<long> &counter,
+                                          tbb::concurrent_bounded_queue<char *> &free_buffers,
+                                          LatencyLogger &event_latency_logger,
+                                          LatencyLogger &processing_latency_logger) :
+      counter_(counter),
+      free_buffers_(free_buffers),
+      event_latency_logger_(event_latency_logger),
+      processing_latency_logger_(processing_latency_logger) {}
+  void dr_cb(RdKafka::Message &message) override {
+    if (message.err()) {
+      exit(1);
+    } else {
+      auto now = std::chrono::steady_clock::now();
+      long after = std::chrono::duration_cast<std::chrono::microseconds>(now.time_since_epoch()).count();
+      long before_event = *(long *) message.msg_opaque();
+      long before_processing = *(((long *) message.msg_opaque()) + 1);
+      event_latency_logger_.Log(after - before_event);
+      processing_latency_logger_.Log(after - before_processing);
+      ++counter_;
+      free_buffers_.push((char *) message.msg_opaque());
+    }
+  }
+ private:
+  std::atomic<long> &counter_;
+  tbb::concurrent_bounded_queue<char *> &free_buffers_;
+  LatencyLogger &event_latency_logger_;
+  LatencyLogger &processing_latency_logger_;
+};
+
+void busy_polling(RdKafka::Producer &producer, std::atomic<bool> &running) {
+  running = true;
+  while (running) {
+    producer.poll(-1);
+  }
+}
+
 
 BenchmarkProducer::BenchmarkProducer(int argc, char *const *argv)
-    : context_p_(std::make_unique<UCP::Context>(true)),
+    : kconfig_(RdKafka::Conf::create(RdKafka::Conf::CONF_GLOBAL)), running_(false),
       free_buffers_p_(std::make_unique < tbb::concurrent_bounded_queue < char * >> ()),
       generated_buffers_p_(std::make_unique < tbb::concurrent_bounded_queue < char * >> ()) {
   const size_t kNumBuffers = 32;
 
   this->ParseOptions(argc, argv);
+  this->ConfigureKafka();
 
-  producer_p_ = DirectProducer::Create(config_, *context_p_);
+  std::string errstr;
+  producer_p_ = std::unique_ptr<RdKafka::Producer>(RdKafka::Producer::create(kconfig_, errstr));
+  if (!producer_p_) {
+    std::cerr << "Failed to create producer: " << errstr << std::endl;
+    exit(1);
+  }
 
   for (size_t _ = 0; _ < kNumBuffers; _++) {
     //TODO: Improved RAII
-    auto pointer = (char *) malloc(GetEffectiveBatchSize());
+    auto pointer = (char *) malloc(config_.max_batch_size);
     free_buffers_p_->push(pointer);
   }
 
@@ -34,11 +98,14 @@ BenchmarkProducer::BenchmarkProducer(int argc, char *const *argv)
                                                1000,
                                                16,
                                                MODE::RELAXED);  // TODO: Adjust mode init
+
+  polling_thread_p_ =  std::make_unique<std::thread>(busy_polling, std::ref(*producer_p_), std::ref(running_));
 }
 
 void BenchmarkProducer::Warmup() {
   std::cout << "Starting warmup ..." << std::endl;
   char *buffer;
+  std::atomic<long> counter = 0l;
   warmup_generator_p_->Start(GetWarmupBatchCount(), *free_buffers_p_, *generated_buffers_p_);
 
   for (size_t count = 0; count < GetWarmupBatchCount(); count++) {
@@ -46,11 +113,15 @@ void BenchmarkProducer::Warmup() {
       std::cout << "Iteration: " << count << std::endl;
 //    }
     generated_buffers_p_->pop(buffer);
-    auto message = std::make_unique<AttachedMessage>(buffer, GetEffectiveBatchSize());
-    producer_p_->Send(1, 1, std::move(message));
-    free_buffers_p_->push(buffer);
+    producer_p_->produce(std::string("benchmark"), RdKafka::Topic::PARTITION_UA, RdKafka::Producer::RK_MSG_BLOCK,
+    buffer, config_.max_batch_size, nullptr, 0, 0, nullptr, buffer);
+    producer_p_->poll(0);
+
   }
   warmup_generator_p_->Stop();
+  while((size_t) counter < GetWarmupBatchCount()) {
+    usleep(10);
+  }
   std::cout << "Finished warmup!" << std::endl;
 }
 
@@ -78,10 +149,10 @@ void BenchmarkProducer::Run() {
     if (!generated_buffers_p_->try_pop(buffer)) {
       throw std::runtime_error("Could not pop from generated buffer, queue is empty.");
     }
-    auto message = std::make_unique<AttachedMessage>(buffer, GetEffectiveBatchSize());
-    producer_p_->Send(1, 1, std::move(message));
-    ++counter;
-    free_buffers_p_->push(buffer);
+    producer_p_->produce(std::string("benchmark"), RdKafka::Topic::PARTITION_UA, RdKafka::Producer::RK_MSG_BLOCK, buffer, config_.max_batch_size, nullptr, 0, 0, nullptr, buffer);
+  }
+  while ((size_t) counter < GetRunBatchCount()) {
+    usleep(10);
   }
   std::cout << "Finishing run execution..." << std::endl;
   auto stop = std::chrono::high_resolution_clock::now();
@@ -90,6 +161,8 @@ void BenchmarkProducer::Run() {
   auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
   std::cout << "Duration: " << duration.count() << " ms" << std::endl;
   generator_p_->Stop();
+  running_ = false;
+  polling_thread_p_->join();
 }
 
 void BenchmarkProducer::ParseOptions(int argc, char *const *argv) {
@@ -105,12 +178,6 @@ void BenchmarkProducer::ParseOptions(int argc, char *const *argv) {
         ("broker-node-port",
          po::value(&config_.broker_node_port)->default_value(13360),
          "Port number of the broker node")
-        ("storage-node-ip",
-         po::value(&config_.storage_node_ip)->default_value("10.150.1.12"),
-         "IP address of the storage node")
-        ("storage-node-port",
-         po::value(&config_.storage_node_port)->default_value(13350),
-         "Port number of the storage node")
         ("batch-size",
          po::value(&config_.max_batch_size)->default_value(131072),
          "Size of an individual batch (sending unit) in bytes")
@@ -122,10 +189,8 @@ void BenchmarkProducer::ParseOptions(int argc, char *const *argv) {
          "The maximum amount of data that is transferred per second")
         ("log-dir",
          po::value(&config_.log_directory)->default_value(
-             "/hpi/fs00/home/hendrik.makait/rembrandt/logs/20200727/e2e/50/exclusive_opt/"),
-         "Directory to store benchmark logs")
-        ("mode", po::value(&mode_str), "The mode in which the producer is run, 'exclusive' or 'concurrent'");
-
+             "/hpi/fs00/home/hendrik.makait/rembrandt/logs/20200727/e2e/50/kafka/"),
+         "Directory to store benchmark logs");
     po::variables_map variables_map;
     po::store(po::parse_command_line(argc, argv, desc), variables_map);
     po::notify(variables_map);
@@ -135,16 +200,37 @@ void BenchmarkProducer::ParseOptions(int argc, char *const *argv) {
       std::cout << desc;
       exit(0);
     }
-    if (mode_str == "exclusive") {
-      config_.mode = Partition::Mode::EXCLUSIVE;
-    } else if (mode_str == "concurrent") {
-      config_.mode = Partition::Mode::CONCURRENT;
-    } else {
-      std::cout << "Could not parse mode: '" << mode_str << "'" << std::endl;
-      exit(1);
-    }
   } catch (const po::error &ex) {
     std::cout << ex.what() << std::endl;
+    exit(1);
+  }
+}
+
+void BenchmarkProducer::ConfigureKafka() {
+  std::string errstr;
+  if (kconfig_->set("bootstrap.servers", config_.broker_node_ip, errstr) != RdKafka::Conf::CONF_OK) {
+    std::cerr << errstr << std::endl;
+    exit(1);
+  }
+
+  if (kconfig_->set("batch.num.messages", "1`", errstr)
+      != RdKafka::Conf::CONF_OK) {
+    std::cerr << errstr << std::endl;
+    exit(1);
+  }
+
+  if (kconfig_->set("message.max.bytes", std::to_string(config_.max_batch_size * 1.1), errstr) != RdKafka::Conf::CONF_OK) {
+    std::cerr << errstr << std::endl;
+    exit(1);
+  }
+
+  if (kconfig_->set("message.copy.max.bytes", "0", errstr) != RdKafka::Conf::CONF_OK) {
+    std::cerr << errstr << std::endl;
+    exit(1);
+  }
+
+  if (kconfig_->set("acks", "-1", errstr) != RdKafka::Conf::CONF_OK) {
+    std::cerr << errstr << std::endl;
     exit(1);
   }
 }
@@ -158,10 +244,6 @@ size_t BenchmarkProducer::GetRunBatchCount() {
 }
 size_t BenchmarkProducer::GetWarmupBatchCount() {
   return GetBatchCount() * config_.warmup_fraction;
-}
-
-size_t BenchmarkProducer::GetEffectiveBatchSize() {
-  return Protocol::GetEffectiveBatchSize(config_.max_batch_size, config_.mode);
 }
 
 int main(int argc, char *argv[]) {
