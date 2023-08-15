4c7dcf755433ed604b329c02f4886ce7f271ae0c
Hendrik Makait <hendrik@makait.com>
2023-08-15 09:44:27 +0200

    Revert "Use IPv6"
    This reverts commit b59a0af028979f34398161b5c35bd4513f0dbabe.

--- include/rembrandt/utils.h
+++ include/rembrandt/utils.h
@@ -9,13 +9,13 @@ typedef std::pair<uint32_t, uint32_t> TopicPartition;
 
 namespace std {
 template<>
-class hash<sockaddr_in6> {
+class hash<sockaddr_in> {
  public:
-  size_t operator()(const sockaddr_in6 &sockaddr_in6) const {
+  size_t operator()(const sockaddr_in &sockaddr_in) const {
     size_t result = 0;
-    boost::hash_combine(result, sockaddr_in6.sin6_family);
-    boost::hash_combine(result, sockaddr_in6.sin6_addr.s6_addr);
-    boost::hash_combine(result, sockaddr_in6.sin6_port);
+    boost::hash_combine(result, sockaddr_in.sin_family);
+    boost::hash_combine(result, sockaddr_in.sin_addr.s_addr);
+    boost::hash_combine(result, sockaddr_in.sin_port);
     return result;
   }
 };
